63758786e5e66e10d95e32358323f049211e8b7b
Mustafa Bursali <m.bursali16@gmail.com>
2023-10-14 00:58:36 +0300

    update

--- src/games/kirmasti/kirmasti.gateway.ts
+++ src/games/kirmasti/kirmasti.gateway.ts
@@ -1,10 +1,15 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway(2001, { namespace: 'kirmasti' })
 export class KirmastiGateway {
   handleConnection(client: Socket) {
-    console.log('user connected');
+    console.log(`${client.id} connected`);
   }
 
   handleDisconnect(client: Socket) {
@@ -16,4 +21,24 @@ export class KirmastiGateway {
     console.log(socket.id);
     return 'Hello world!';
   }
+
+  // JOIN ROOM
+  @SubscribeMessage('join-room')
+  async joinRoom(@ConnectedSocket() socket, @MessageBody() body) {
+    console.log('Received join-room event:', body);
+    if (socket) {
+      await socket.join(body.room);
+      console.log(`${socket.id} has joined to room ${body.room}.`);
+    } else {
+      console.log('Socket object is undefined.');
+    }
+  }
+
+  @SubscribeMessage('accept-deal')
+  async acceptDeal() {}
+
+  @SubscribeMessage('bet')
+  async bet() {}
+
+  //
 }
