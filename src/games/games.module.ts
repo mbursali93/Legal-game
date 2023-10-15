8d2545c5276f22755acd08934931380cc00a52d9
Mustafa Bursali <m.bursali16@gmail.com>
2023-10-16 01:05:32 +0300

    module changed

--- src/games/games.module.ts
+++ src/games/games.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { KirmastiController } from './kirmasti/kirmasti.controller';
+import { KirmastiService } from './kirmasti/kirmasti.service';
+import { KirmastiGateway } from './kirmasti/kirmasti.gateway';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Kirmasti, KirmastiSchema } from 'src/schemas/kirmasti.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Kirmasti.name, schema: KirmastiSchema },
+    ]),
+  ],
+  controllers: [KirmastiController],
+  providers: [KirmastiService, KirmastiGateway],
+})
 export class GamesModule {}
